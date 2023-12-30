56bdba34982f0afd4d4ffc9dd7e5e44895892fe4
Théa G <50581929+feyd-eu@users.noreply.github.com>
2023-12-30 13:45:02 +0100

    Update queries.sql
    Added Step 5.3 (with an issue), see line 37

--- queries.sql
+++ queries.sql
@@ -16,3 +16,27 @@ left join products p
 group by concat(e.first_name, ' ', e.last_name)             -- группируем данные по имени продавца
 order by income desc                                        -- ранжирум по доходу от большего к меньшему    
 LIMIT 10                                                    -- оставляем только 10 первых результатов    
+
+
+/** Step 5.3 **/
+with tab as (select                                         -- создаем CTE для последующей работы
+    s.quantity,                                             -- выбираем количество проданных товаров
+    p.price,                                                -- выбираем цену каждого товара
+    concat(e.first_name, ' ', e.last_name) as name,         -- cоставляем полное имя продавца из его имени и фамилии
+    s.sale_date as date,                                    -- выбираем дату продажи
+    to_char(s.sale_date, 'day') as weekday                  -- превращаем дату продажи в день недели, записанный с маленькой буквы
+from employees e                                            -- выбираем данные из таблицы employees; объединяем ее с таблицами sales и products
+left join sales s
+  on s.sales_person_id = e.employee_id
+left join products p
+  on p.product_id = s.product_id
+group by e.employee_id, s.customer_id, s.sale_date, s.quantity, p.price, p.name) -- группируем данные по всем указанным ранее параметрам
+
+select
+	distinct t.name,                                          -- выбираем полное имя продавца из CTE; distinct помогает избавиться от дублирующихся строк
+	t.date,                                                   -- выбираем дату продажи (к сожалениию, не понимаю, как обойтись без нее в целях сортировки)
+	t.weekday,                                                -- выбираем день продажи
+	round(sum(t.quantity * t.price) over (partition by t.name order by t.date),0) as income -- считаем суммарный доход по продавцу на каждую из дат
+from tab as t                                               -- выбираем данные из CTE tab
+group by t.date, weekday, t.name, t.quantity, t.price       -- группируем данные по всем указанным ранее параметрам
+order by t.date, t.name                                     -- сортируем данные по дате и имени продавца   
